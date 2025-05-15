49de0aeb27fc847dfea3772320b31097d88ce4b8
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2025-05-15 12:34:03 +0100

    20250516

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import "./index.css";
 function App() {
   return (
     <>
-      <h1 className="text-amber-500">Hello world!</h1>
+      <div className="bg-violet-200 h-10 w-full border-2 border-violet-600 rounded-md my-4 p-2">
+        <h1 className="text-black-500 text-center font-mono font-extrabold">
+          Hello world!
+        </h1>
+      </div>
     </>
   );
 }
