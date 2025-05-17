30b4ad978affee12935045a04063a7ba719eb7d6
Chon Lou <163328241+ChonCanCode@users.noreply.github.com>
2025-05-17 11:39:14 +0100

    20250517

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,19 @@ import "./index.css";
 function App() {
   return (
     <>
-      <h1 className="text-amber-500">Hello world!</h1>
+      <div className=" flex flex-col items-center justify-between border-amber-400 border-2 space-y-6 p-5 mt-2">
+        <h1 className="">I am flexbox</h1>
+        <div className="h-16 w-16 rounded-full bg-blue-500"></div>
+        <div className="h-16 w-16 rounded-full bg-orange-500"></div>
+        <div className="h-16 w-16 rounded-full bg-green-500"></div>
+      </div>
+
+      <div className="grid border-2 border-amber-500 grid-cols-4">
+        <h1>I am grid</h1>
+        <div className="h-16 w-16 rounded-full bg-blue-500"></div>
+        <div className="h-16 w-16 rounded-full bg-orange-500"></div>
+        <div className="h-16 w-16 rounded-full bg-green-500"></div>
+      </div>
     </>
   );
 }
