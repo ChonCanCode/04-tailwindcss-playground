9efd68fd4b6e5e8ccab7aee34dd5ce3246a83fc4
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2025-05-20 20:00:48 +0100

    messing with repoonsive

--- src/Responsive.tsx
+++ src/Responsive.tsx
@@ -1,8 +1,12 @@
 export default function Responsive() {
   return (
     <>
-      <div className="sm:bg-amber-200 max-md:bg-amber-700">
-        <p>I appear screen wider than 768px</p>
+      <div className="sm:bg-amber-200 md:bg-amber-700 text-center p-5">
+        <p className="font-bold">I am using sm & md prefix</p>
+      </div>
+
+      <div className="max-[600px]:bg-blue-600 min-[320px]:text-center min-[320px]:bg-blue-400 p-5">
+        <p className="font-bold">I am using arbitrary value</p>
       </div>
     </>
   );
