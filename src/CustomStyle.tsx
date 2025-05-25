f6ada2e57908df1603c50d5d5db07d541626cd26
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2025-05-25 13:40:45 +0100

    learned @layer base and components

--- src/CustomStyle.tsx
+++ src/CustomStyle.tsx
@@ -3,7 +3,7 @@ export default function CustomStyle() {
     <>
       <h1>Custome Style</h1>
       <p className="bg-stone-500 text-[34px] text-[#df9684]">In-line tyle</p>
-      <p className="text-piggy">Tailwind config</p>
+      <p className="card">Tailwind config</p>
     </>
   );
 }
